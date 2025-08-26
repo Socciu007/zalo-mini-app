64d000cca7fda84cd8db0ec5dff10698b11aba18
Money <manhtien310701@gmail.com>
2025-08-26 18:52:21 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,24 @@
-// vite.config.ts
-import { defineConfig, type UserConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
-  const isDev = command === 'serve'
-
-  const config: UserConfig = {
-    base: './',
+// https://vitejs.dev/config/
+export default () => {
+  return defineConfig({
+    root: "./",
+    base: "",
     plugins: [tsconfigPaths(), react()],
     build: {
-      target: 'esnext',
-      outDir: 'dist'
-    },
-    esbuild: {
-      target: 'esnext',
-      supported: { 'import-meta': true }
-    },
-    optimizeDeps: {
-      entries: ['src/app.tsx'],
-      include: ['react', 'react-dom', 'zmp-ui', 'recoil'],
-      esbuildOptions: {
-        target: 'esnext',
-        supported: { 'import-meta': true }
-      }
+      target: "esnext",
+      outDir: "dist",
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            return id.includes("node_modules") ? "vendor" : undefined;
+          }
+        }
+      },
+      chunkSizeWarningLimit: 1000
     }
-  }
-
-  return config
-})
+  });
+};
